bc2dac9e16d60fb9920bea070d22f4cc0b5e6f76
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-08-27 22:02:55 +0100

    api not working

--- src/script.js
+++ src/script.js
@@ -1,32 +1,5 @@
 // const image=document.createElement("span")
 
-// const options = {
-//   method: "GET",
-//   headers: {
-//     accept: "application/json",
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NmRmNDlkOWFmMWJjMTFmYWI4MDE0MGVhO
-//DM0N2IyMCIsInN1YiI6IjY0ZWE1MDdjNTI1OGFlMDE0ZGYyZDEyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iW2S4RopStbFn_abwAoapxsTQlMH - 4nHDzpACiSRfRQ",
-//   },
-// };
-
-// fetch("https://api.themoviedb.org/3/configuration", options)
-//   .then((response) => response.json())
-//     .then((response) => {
-//         console.log(response);
-//         const imageSource = response.change_keys;
-//         console.log(imageSource);
-
-//         //loop through imageSource and assign each value to image?
-//         for (let i = 0; i < imageSource.length; i++) {
-//           let image = new Image();
-//           image.src = imageSource[i];
-//           document.appendChild(image);
-//         }
-
-//     })
-//   .catch((err) => console.error(err));
-
 // const span = document.querySelector("span")
 // image.append("span")
 
