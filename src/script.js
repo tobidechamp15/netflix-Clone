7ec88d5463de25d423fd304eeb7f128328cbb697
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-08-29 11:08:45 +0100

    api now working

--- src/script.js
+++ src/script.js
@@ -1,13 +1,13 @@
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    accept: "application/json",
+    accept: 'application/json',
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NmRmNDlkOWFmMWJjMTFmYWI4MDE0MGVhODM0N2IyMCIsInN1YiI6IjY0ZWE1MDdjNTI1OGFlMDE0ZGYyZDEyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iW2S4RopStbFn_abwAoapxsTQlMH-4nHDzpACiSRfRQ",
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NmRmNDlkOWFmMWJjMTFmYWI4MDE0MGVhODM0N2IyMCIsInN1YiI6IjY0ZWE1MDdjNTI1OGFlMDE0ZGYyZDEyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iW2S4RopStbFn_abwAoapxsTQlMH-4nHDzpACiSRfRQ',
   },
 };
 
-fetch("https://api.themoviedb.org/3/trending/all/day?language=en-US", options)
+fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
   .then((response) => response.json())
   .then((response) => {
     console.log(response);
