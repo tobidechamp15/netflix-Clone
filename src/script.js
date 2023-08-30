231a30cf2b1d33cd1c461f13f1df6b656bf3dd70
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-08-30 13:57:39 +0100

    Trending created

--- src/script.js
+++ src/script.js
@@ -9,6 +9,36 @@ const options = {
 
 fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
   .then((response) => response.json())
-  .then((response) => {
-    console.log(response);
+  .then((data) => {
+    const datum = data.results;
+    datum.map((item) => {
+      const poster = item.poster_path;
+      const fullPosterUrl = `https://image.tmdb.org/t/p/original/${poster}`;
+      const movie = `<li class="flex flex-col gap-3 "><img class="posterImg"  src="${fullPosterUrl}"></li>`;
+      document.querySelector('.movies').innerHTML += movie;
+      const eachMovie = document.querySelectorAll('.posterImg');
+      const oneMovie = [];
+      oneMovie.push(eachMovie);
+      const e = oneMovie[0];
+
+      e.forEach((i) => {
+        i.addEventListener('click', () => {
+          window.location.href = './movieDetails.html';
+        });
+      });
+
+      return true;
+    });
   });
+
+const menuBar = document.querySelector('.ham');
+menuBar.addEventListener('click', () => {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('active');
+});
+
+const cancel = document.querySelector('.cancel');
+cancel.addEventListener('click', () => {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('active');
+});
