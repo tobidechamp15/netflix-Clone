33d02ab4b745900e6d2cdba2ea625c51a68e56a7
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-09-02 00:17:23 +0100

    Dev review

--- src/script.js
+++ src/script.js
@@ -6,35 +6,79 @@ const options = {
       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NmRmNDlkOWFmMWJjMTFmYWI4MDE0MGVhODM0N2IyMCIsInN1YiI6IjY0ZWE1MDdjNTI1OGFlMDE0ZGYyZDEyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iW2S4RopStbFn_abwAoapxsTQlMH-4nHDzpACiSRfRQ',
   },
 };
+function fetchingMovie() {
+  fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+    .then((response) => response.json())
+    .then((data) => {
+      const datum = data.results;
+      datum.forEach((item) => {
+        const arrayOfId = [];
 
-fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
-  .then((response) => response.json())
-  .then((data) => {
-    const datum = data.results;
-    datum.forEach((item) => {
-      const arrayOfId = [];
+        const eachMovieId = item.id;
 
-      const eachMovieId = item.id;
+        arrayOfId.push(eachMovieId);
 
-      arrayOfId.push(eachMovieId);
+        const poster = item.poster_path;
+        const fullPosterUrl = `https://image.tmdb.org/t/p/original/${poster}`;
+        const movie = `<li class="flex flex-col gap-3 "><section  class="imageContainer">
+        <img id=${eachMovieId}  class="posterImg"  src="${fullPosterUrl}"></section></li>`;
+        document.querySelector('.movies').innerHTML += movie;
 
-      const poster = item.poster_path;
-      const fullPosterUrl = `https://image.tmdb.org/t/p/original/${poster}`;
-      const movie = `<li class="flex flex-col gap-3 "><section  class="imageContainer"><img id=${eachMovieId}  class="posterImg"  src="${fullPosterUrl}"></section></li>`;
-      document.querySelector('.movies').innerHTML += movie;
-      const posterImages = document.querySelectorAll('.posterImg');
-      posterImages.forEach((posterImg) => {
-        posterImg.addEventListener('click', () => {
-          const movieId = posterImg.id;
+        const posterImages = document.querySelectorAll('.posterImg');
+        posterImages.forEach((posterImg) => {
+          posterImg.addEventListener('click', () => {
+            const movieId = posterImg.id;
 
-          // Fetch and display video content based on movieId
-          const movieUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`;
-          fetch(movieUrl, options)
-            .then((response) => response.json())
-            .then((data) => data);
+            // Fetch and display video content based on movieId
+            const movieUrl = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
+            fetch(movieUrl, options)
+              .then((response) => response.json())
+              .then((data) => {
+                if (data.backdrop_path !== null) {
+                  const backDropImagePath = data.backdrop_path;
+                  const backDropImageUrl = `https://image.tmdb.org/t/p/original/${backDropImagePath}`;
+                  const backDropImage = document.querySelector('.selectedBackdrop');
+                  backDropImage.src = backDropImageUrl;
+                  const selectedTitle = data.original_title;
+                  const titleContainer = document.querySelector('.titleContainer');
+                  titleContainer.textContent = selectedTitle;
+                  const overviewContent = data.overview;
+                  const details = document.querySelector('.details');
+                  details.textContent = overviewContent;
+
+                  const genreArray = data.genres;
+
+                  genreArray.forEach((item) => {
+                    const genreList = document.createElement('li');
+                    genreList.textContent = `${item.name}  |`;
+                    const genres = document.querySelector('.genre');
+                    genres.append(genreList);
+                  });
+
+                  // const movieDetails = document.querySelector('.movieDetails');
+                  // movieDetails.append(backDropImage);
+
+                  const mainContent = document.querySelector('.mainContent');
+                  const movieDetails = document.querySelector('.movieDetails');
+                  movieDetails.classList.remove('-top-full');
+                  movieDetails.classList.add('easeIn');
+                  mainContent.style.opacity = '20%';
+                  mainContent.addEventListener('click', () => {
+                    movieDetails.classList.add('-top-full');
+                    mainContent.style.opacity = '100%';
+                    movieDetails.classList.remove('easeIn');
+                    movieDetails.classList.add('ease-out', 'duration-1000');
+                  });
+                } else {
+                  return true;
+                }
+                return 0;
+              });
+          });
         });
-      });
 
-      return true;
+        return true;
+      });
     });
-  });
+}
+fetchingMovie();
